5fb36c0406a97d1e7a0674a0cb5965d37981db13
rjsdn031 <rjsdn031@naver.com>
2024-12-06 00:00:54 +0900

    HOTFIX: 실행 시 타입 오류 수정
    에이전트 구조 변경
    process_state 에이전트 내부로 이동
    저장 방식 변경 - pickle 사용, filename에 따라 다른 이름으로 저장
    
    업데이트 주기(TARGET_UPDATE_FREQ) 수정 -> 임의 변경 가능(현재 20)
    학습 에피소드 수정 -> 임의 변경 가능 (200회 기준 약 15000초).
    학습에 걸리는 시간 측정
    보상 방식  일부 수정

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
+import pickle
 from knu_rl_env.road_hog import evaluate, run_manual
+from DQN_base import *
+
+def load(filename):
+    try:
+        with open(f'{filename}.pickle', 'rb') as fr:
+            agent = pickle.load(fr)
+        return agent
+    except FileNotFoundError:
+        print(f"Error: File '{filename}.pickle' not found.")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 if __name__ == '__main__':
-    # agent = '''여러분이 정의하고 학습시킨 에이전트를 불러오는 코드를 넣으세요.'''
-    # evaluate(agent)
-    run_manual()
+    agent = load('dqn_test2')
+    evaluate(agent)
+    # run_manual()
